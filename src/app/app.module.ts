20bd2f5971e0567c555c2821314298dc6143f62d
EsercitoHaki <esercitohaki2003@gmail.com>
2024-11-24 21:57:43 +0700

    update giao diện

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,62 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DetailProductComponent } from './components/detail-product/detail-product.component';
+import { OrderComponent } from './components/order/order.component';
+import { OrderDetailComponent } from './components/detail-order/order.detail.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  provideHttpClient, 
+  HTTP_INTERCEPTORS, 
+  HttpClientModule
+} from '@angular/common/http';
+import { TokenInterceptor } from './interceptors/token.interceptors';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
+
+  declarations: [    
+    HomeComponent, 
+    HeaderComponent,
+    FooterComponent, 
+    DetailProductComponent, 
+    OrderComponent, 
+    OrderDetailComponent, 
+    LoginComponent, 
+    RegisterComponent, 
+    AppComponent, 
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [
-    provideClientHydration()
+  providers: [provideHttpClient(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    //HomeComponent,
+    //DetailProductComponent,
+    //OrderComponent,
+    //OrderDetailComponent,
+    //LoginComponent,
+    //RegisterComponent
+  ]
 })
 export class AppModule { }
